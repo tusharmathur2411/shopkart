3175ac87f573581d6e31a13180c4cadbd2672d12
tusharmathur2411 <55529238+tusharmathur2411@users.noreply.github.com>
2022-01-16 11:32:49 +0530

    initial commit

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,46 +1,60 @@
-import { combineReducers } from 'redux'
+import { combineReducers } from "redux";
 
-const currUser = JSON.parse(sessionStorage.getItem("user"))
+const currUser = JSON.parse(sessionStorage.getItem("user"));
 
-function orders(state=[], action) {
-    switch (action.type) {
-        case "LOAD_ORDERS": return action.orders
-        case "LOGOUT": return []
-        default: return state
-    }    
+function orders(state = [], action) {
+  switch (action.type) {
+    case "LOAD_ORDERS":
+      return action.orders;
+    case "LOGOUT":
+      return [];
+    default:
+      return state;
+  }
 }
 
-function cartLoading(state=false, action) {
-    switch (action.type) {
-        case "CART_LOADING": return true
-        case "LOAD_CART": return false
-        default: return state
-    }
+function cartLoading(state = false, action) {
+  switch (action.type) {
+    case "CART_LOADING":
+      return true;
+    case "LOAD_CART":
+      return false;
+    default:
+      return state;
+  }
 }
 
-function user(state=currUser, action) {
-    console.log(action)
-    switch (action.type) {
-        case "LOGIN": return action.user
-        case "LOGOUT": return null
-        case "UPDATE_NAME": return {...state, displayName: action.updatedName}
-        default: return state
-    }    
+function user(state = currUser, action) {
+  switch (action.type) {
+    case "LOGIN":
+      return action.user;
+    case "LOGOUT":
+      return null;
+    case "UPDATE_NAME":
+      return { ...state, displayName: action.updatedName };
+    default:
+      return state;
+  }
 }
 
-function products(state=[], action) {
-    switch (action.type) {
-        case "LOAD_PRODUCTS": return action.products
-        default: return state
-    }
+function products(state = [], action) {
+  switch (action.type) {
+    case "LOAD_PRODUCTS":
+      return action.products;
+    default:
+      return state;
+  }
 }
 
-function cart(state=[], action) {
-    switch (action.type) {
-        case "LOAD_CART": return action.cart
-        case "LOGOUT": return JSON.parse(localStorage.getItem("cart"))
-        default: return state
-    }
+function cart(state = [], action) {
+  switch (action.type) {
+    case "LOAD_CART":
+      return action.cart;
+    case "LOGOUT":
+      return JSON.parse(localStorage.getItem("cart"));
+    default:
+      return state;
+  }
 }
 
-export default combineReducers({cartLoading, user, products, cart, orders})
+export default combineReducers({ cartLoading, user, products, cart, orders });
